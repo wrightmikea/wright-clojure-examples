e439cfc86004171c901315cba8af88dff0e6762a
Michael A. Wright <wrightmikea@gmail.com>
2010-01-16 19:19:37 -0800

    clean up

--- bottles4.clj
+++ bottles4.clj
@@ -16,15 +16,3 @@
 	       (list (bob 1 otw)(bob 1 ".\n")(tod "it" "No more")))))))
 
 
-
-(comment
-  (let [
-	   beers-wall (repeatedly #(str bottle "s " beer " " wall ". "))
-	   before (repeatedly #(str bottle "s " beer ".\n"))
-	   act (repeatedly #(str take " one " down))
-	   after (repeatedly #(str bottle "s " beer " " wall ".\n"))]
-	 (print (apply str (interleave r1 beers-wall r1 before act r2 after))
-		(str "1 " bottle " " beer " " wall ", "
-		     "1 " bottle " " beer".\n"
-		     take " it " down " "
-		     "No more " bottle "s " beer " " wall "!\n"))))
