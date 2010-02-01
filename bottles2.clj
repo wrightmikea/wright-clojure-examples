c4b5d8370d452648b9b799b9d18f0ffcc315a4b1
Mike Wright <wrightmikea@gmail.com>
2010-02-01 10:06:32 -0800

    update trampoline example to use letfn and defn-

--- bottles2.clj
+++ bottles2.clj
@@ -1,18 +1,19 @@
-(defn bottles [n]
+(defn- bottles [n]
   (str n " bottle" (if (= 1 n) "" "s")))
-(declare even)
-(defn odd [n]
-  (if (pos? n)
-    (do
-      (println (bottles n) "of beer on the wall," (bottles n) "of beer.")
-      #(even (dec n)))
-    nil))
-(defn even [n]
-    (do
-      (println "Take one down and pass it around," (bottles n) "of beer on the wall.\n")
-      (if (pos? n)
-	#(odd n)
-	nil)))
-(defn sing
-  ([] (sing 99)) ; default
-  ([n] (trampoline #(odd n))))
+(letfn [ (odd [n]
+	      (if (pos? n)
+		(do
+		  (println (bottles n) "of beer on the wall," 
+			   (bottles n) "of beer.")
+		  #(even (dec n)))
+		nil))
+	 (even [n]
+	       (do
+		 (println "Take one down and pass it around," 
+			  (bottles n) "of beer on the wall.\n")
+		 (if (pos? n)
+		   #(odd n)
+		   nil)))]
+  (defn sing
+    ([] (sing 99)) ; default
+    ([n] (trampoline #(odd n)))))
